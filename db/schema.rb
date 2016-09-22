b01aaa30b20250a9a905e28be8c9a1ed11e73f15
tryc <terashima.yuko@gmail.com>
2016-09-22 07:04:20 +0000

    Add friend scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922044653) do
+ActiveRecord::Schema.define(version: 20160922065820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160922044653) do
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string   "name"
+    t.string   "team"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
